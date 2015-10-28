6d6eeed0baee44fd3db167cfa5e0a814c361cb46
panhc <874014861@qq.com>
2015-10-28 10:26:32 +0800

    在线答疑功能3

--- app/controllers/teachers/quest.controller.js
+++ app/controllers/teachers/quest.controller.js
@@ -1,16 +1,32 @@
+'use strict'
 
+var mongoose = require('mongoose'),
+    _ = require('lodash'),
+    teacherMenus = require('../../common/menus').teacherMenus,
+    Question = mongoose.model('Question');
 /**
  * 问题列表页面
  * @param req
  * @param res
  * @returns {*}
  */
+
 exports.question = function (req, res) {
     return res.render('backend/teacher/questions/list-questions');
 };
-
-exports.questioninfo = function (req, res) {
-    return res.render('backend/teacher/questions/view-quest');
+/**
+ * 问题详情页面
+ *
+ *
+ * */
+exports.questioninfo = function (req, res, next ) {
+    var questionId = req.params.questionId;
+    Question.findById(questionId,function(err,question){
+        if(err){
+            return next(err);
+        }
+        return res.render('backend/teacher/questions/view-quest',{ question:question});
+    });
 };
 
 
