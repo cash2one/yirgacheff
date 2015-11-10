233c9e58bc734a49abe2a806ccbca5be699ef78d
Frank <kuando@126.com>
2015-11-10 12:57:43 +0800

    bootstrapTable

--- public/js/app/teacher/quizBase/list-quizzes.js
+++ public/js/app/teacher/quizBase/list-quizzes.js
@@ -1,15 +1,60 @@
 'use strict';
 
+
 requirejs(['jquery', 'leftMenu', 'headMenu', 'easyDropDown', 'datatables'],
     function ($, leftMenu, headMenu) {
-            leftMenu.init();
-            headMenu.init();
-            $('#library-list').dataTable({
-                'bAutoWidth': true,
-                "bSort": false,
-                'language': {
-                    'url': '/js/lib/plugins/dataTable/Chinese.lang'
-                }
-            });
+        leftMenu.init();
+        headMenu.init();
+        function formatDate(data) {
+            var date = new Date(data);
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var day = date.getDay();
+            return year + '-' + month + '-' + day;
+        }
 
+        var jqTable = $('#library-list');
+        var dataTable = jqTable.DataTable({
+            'bAutoWidth': true,
+            'bSort': false,
+            'language': {
+                'url': '/js/lib/plugins/dataTable/Chinese.lang'
+            },
+            'processing': true,
+            'serverSide': true,
+            'ajax': {
+                url: '/api/v1/quizzes',
+                dataSrc: 'data'
+            },
+            'order': [[1, "desc"]],
+            'columns': [
+                {'data': 'title'},
+                {'data': 'title'},
+                {'data': 'creatorDisplayName'},
+                {'data': 'createdTime'},
+                {'data': 'usage'},
+                {'data': '_id'}
+            ],
+            'columnDefs': [
+                {
+                    'targets': [3],
+                    'render': function (data) {
+                        return formatDate(data);
+                    }
+                },
+                {
+                    'targets': [0],
+                    'render': function (data, type, row, meta) {
+                        return meta.row + 1;
+                    }
+                },
+                {
+                    'targets': [5],
+                    'render': function (data, type, row, meta) {
+                        return '<a class="btn-small a-btn-blue detail" href=" /teacher/quizzes/' + data + '">查看</a> ' +
+                            '<a class="btn-small a-btn-red delete-btn" href="/teacher/homework/create?quizId=' + data + '">使用</a>';
+                    }
+                }
+            ]
+        });
     });
