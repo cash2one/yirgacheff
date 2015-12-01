e779d9bdfe53ecdde845b021c11b15eeece3a5df
Frank <kuando@126.com>
2015-12-01 18:58:40 +0800

    version info

--- public/js/config.js
+++ public/js/config.js
@@ -3,7 +3,7 @@
  */
 requirejs.config({
     baseUrl: '/js',
-    urlArgs: 'v=2',
+    urlArgs: 'v=3',
     paths: {
         // lib
         'jquery': ['//cdn.staticfile.org/jquery/1.11.1/jquery.min', 'lib/jquery'],
