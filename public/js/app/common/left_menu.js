448516e3fc00daa39edbbb92c234ebe58f06fcfa
zgx1993 <273994140@qq.com>
2015-12-21 18:55:07 +0800

    页面修改

--- public/js/app/common/left_menu.js
+++ public/js/app/common/left_menu.js
@@ -9,31 +9,31 @@ define(['jquery', 'layer', 'lazyLoad','slimscroll'], function ($, layer) {
         $("img.lazy").lazyload({
             effect: "fadeIn"
         });
-        //$(document).ready(function(){
-        //    var height = document.body.offsetHeight;
-        //    $(".page-sidebar").height(height);
-        //});
         //处理左侧菜单栏的选中状态
-        var sideMenu = $(".m-side-menu");
+        var sideMenu = $(".left-menu").find("li.active").length;
+        if(sideMenu > 0)
+        {
+            $('ul').find("li.active").parent().collapse('show')
+        }
 
         //手风琴效果展示
-        sideMenu.find("ul li.pull-down").click(function () {
-            var $ink = $(this).find(".one-level");
-            var $obj = $(this).find(".pull-level");
-            var view = $obj.is(":visible");
-            if (!view) {
-                $obj.css('display', 'block');
-                $ink.css('background-position', '200px 27px');
-            }
-            else {
-                $obj.css('display', 'none');
-                $ink.css('background-position', '200px -5px');
-            }
-            $(this).siblings(".pull-down").
-                find(".pull-level").css('display', 'none');
-            $(this).siblings(".pull-down").
-                find(".one-level").css('background-position', '200px -5px');
-        });
+        //sideMenu.find("ul li.pull-down").click(function () {
+        //    var $ink = $(this).find(".one-level");
+        //    var $obj = $(this).find(".pull-level");
+        //    var view = $obj.is(":visible");
+        //    if (!view) {
+        //        $obj.css('display', 'block');
+        //        $ink.css('background-position', '200px 27px');
+        //    }
+        //    else {
+        //        $obj.css('display', 'none');
+        //        $ink.css('background-position', '200px -5px');
+        //    }
+        //    $(this).siblings(".pull-down").
+        //        find(".pull-level").css('display', 'none');
+        //    $(this).siblings(".pull-down").
+        //        find(".one-level").css('background-position', '200px -5px');
+        //});
 
 
         //处理反馈信息
