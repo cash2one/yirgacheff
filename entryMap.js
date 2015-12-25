72caff9ae02c70b1c517d27f68254abd11ee3605
michaelegend <michaelegend@gmail.com>
2015-12-25 13:57:25 +0800

    wepback-feature update

---
+++ entryMap.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2015/12/23.
+ */
+
+'use strict';
+
+var entryMap = {
+    "/assets/bundles/views/backend/school/home":                          __dirname + "/entry/views/backend/school/home.entry",
+    "/assets/bundles/views/backend/school/manager/list-classes":          __dirname + "/entry/views/backend/school/manager/list-classes.entry",
+}
+module.exports = entryMap;
