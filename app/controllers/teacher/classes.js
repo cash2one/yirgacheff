28aa5de35a6c7e6b2cb66c8bc81f7221b85d81a4
zgx1993 <273994140@qq.com>
2015-12-31 15:16:32 +0800

    teacher端修改

--- app/controllers/teacher/classes.js
+++ app/controllers/teacher/classes.js
@@ -2,11 +2,18 @@
  * Created by Frank on 15/12/22.
  */
 'use strict';
+const service = require('../../services');
 
 module.exports = function (router) {
 
     router.get('/', function*() {
         yield this.render('backend/teacher/manager/list-classes');
+    });
+
+    router.get('/:id',function*(){
+        this.state.clazz = yield service.classes.findById(this.params.id,true);
+        this.state.students = yield service.students.findByClass(this.params.id);
+        yield this.render('backend/teacher/manager/list-students');
     });
 
     return router;
