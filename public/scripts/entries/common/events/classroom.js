4c9b503fd2d0aa63a35e038945a2663ac5bb1ec8
zgx1993 <273994140@qq.com>
2016-01-12 14:46:40 +0800

    在线课堂

--- public/scripts/entries/common/events/classroom.js
+++ public/scripts/entries/common/events/classroom.js
@@ -6,16 +6,11 @@ require('../../../common/formvalidator');
 var app = require('../../../common/app');
 var notify = require('../../../common/notify');
 var upload = require('../../../common/uploadifive');
-var richEditor = require('../../../common/richEditor');
-var weixinEditor = require('../../../common/weixinEditor');
 
 $(document).ready(function () {
     app();
 
-    //富编辑器渲染
-    var editor = richEditor.render('content');
-    //初始化微信编辑器
-    weixinEditor({editor: editor});
+    var $videoItem =$(".video-item");
 
     upload({
         file: 'imageUpload',
@@ -23,6 +18,39 @@ $(document).ready(function () {
             $('#indexImage').attr('src', file.path);
             $('#coverImage').val(file.key);
         }
+    });
+
+    $("#uploadVideo").on('click', '.upload', function () {
+        $("#addVideoModal").modal('show');
+    });
+
+    $("#addVideoForm").validate(function () {
+        var link = $("#link").val();
+        var name = $("#videoName").val();
+        var detail =$("#videoDetail").val();
+        var video = "<div class='media b m-b-md'>" +
+            "<div class='media-left video-container'>"+ link +"</div>" +
+            "<div class='media-body'><p class='video-title'>"+ name +"</p>" +
+            "<div class='video-control'><a class='f-info m-r-xs reset-video'>重新上传</a><a class='f-pink delete-video'>删除</a></div>" +
+            "<p class='video-detail'>"+ detail +"</p></div></div>";
+        $(".video-item").append(video);
+        $("#addVideoModal").modal('hide');
+        notify.success("添加成功");
+        $("#addVideoForm")[0].reset();
+    });
+
+    //重新上传
+    $videoItem.on('click', '.reset-video', function () {
+        var link = $(this).closest('.media').find('.video-container').html();
+        var name = $(this).closest('.media').find('.video-title').html();
+        var detail = $(this).closest('.media').find('.video-detail').html();
+        console.log(link,name,detail)
+    });
+
+    //删除视频
+    $videoItem.on('click', '.delete-video', function () {
+        $(this).closest('.media').remove();
+        notify.danger("删除成功");
     });
 
 });
