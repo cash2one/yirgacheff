491afb6b222c1e36ffccb6356dd78aa069b6bb68
zgx1993 <273994140@qq.com>
2016-01-25 21:30:58 +0800

    报名名单

--- public/scripts/entries/common/events/manage-enroll.js
+++ public/scripts/entries/common/events/manage-enroll.js
@@ -9,13 +9,25 @@ $(document).ready(function () {
     $('#enrollTable').DataTable({
         'bAutoWidth': true,
         'bSort': false,
+        "sDom": 'T<"clear">lfrtip',
+        "oTableTools": {
+            "aButtons":[
+                {
+                    "sExtends":"xls",
+                    "sFileName": "报名名单.xls",
+                    "sButtonText":"导出到Excel"
+                }
+            ],
+            "sSwfPath": "//cdn.bootcss.com/datatables-tabletools/2.1.5/swf/copy_csv_xls_pdf.swf"
+        },
         'language': {
             'url': '/js/lib/plugins/dataTable/Chinese.lang'
         }
     });
 
-    $("#exportExcel").click(function () {
-        window.open('/school/events/manage/' + eventId + '/enroll/excel', "_self");
-    });
+    //$("#exportExcel").click(function () {
+    //    //window.open('/school/events/manage/' + eventId + '/enroll/excel', "_self");
+    //    //$('#enrollTable').tableExport({type:'excel',escape:'false'});
+    //});
 
 });
