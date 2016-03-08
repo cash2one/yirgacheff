92f12bac0276a4b3de5cff563010c7128d414b86
Frank <kuando@126.com>
2016-03-08 11:54:37 +0800

    活动名称优化

--- config/swigFilters.js
+++ config/swigFilters.js
@@ -10,6 +10,24 @@ module.exports = {
             return input.substring(0, length) + '...';
         }
         return input;
+    },
+
+    templateName: function (input) {
+        if (input === 'article') {
+            return '文章';
+        }
+        if (input === 'activity') {
+            return '活动';
+        }
+        if (input === 'vote') {
+            return '投票';
+        }
+        if (input === 'classroom') {
+            return '在线课堂';
+        }
+        if (input === 'audition') {
+            return '试听课';
+        }
     }
 };
 
